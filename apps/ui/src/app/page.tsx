d91273152fc38e214eda41afc2c2b8b934ea9f88
Matthew Schlenker <m4ttschlenker@gmail.com>
2025-04-23 15:20:11 -0700

    Remove tailwind from monorepo.

--- apps/ui/src/app/page.tsx
+++ apps/ui/src/app/page.tsx
@@ -2,7 +2,6 @@ export default function Index() {
   /*
    * Replace the elements below with your own.
    *
-   * Note: The corresponding styles are in the ./index.tailwind file.
    */
   return (
     <div>
